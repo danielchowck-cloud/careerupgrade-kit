6e47ca5e2171f36d75df692c8895661733de53e8
Daniel <dcck888@Daniels-MacBook-Neo.local>
2026-07-16 12:09:26 +0800

    Remove beta rollout wording

--- stripe-config.js
+++ stripe-config.js
@@ -1,6 +1,6 @@
 window.CUK_STRIPE_CONFIG = {
   paymentLink: "https://buy.stripe.com/14AaEY9Z83I016wfC8gA801",
   successUrl: "payment-success.html",
-  productName: "CareerUpgrade Kit Beta",
+  productName: "CareerUpgrade Kit",
   amountLabel: "SGD 9.90"
 };
